0ff903a941b197e248f5050ce0426a32c0904873
Aman Sharma <mannu.poski10@gmail.com>
2023-07-06 15:58:51 +0200

    Refactor Terminator.java to log class names
    Refactored Terminator.java to record and log loaded class names for future analysis. The old code was removed which was checking for a prohibited class and replacing frames in the bytecode using a ByteBuddy library. The new implementation now logs the class names into a local file and stores them into a list.

--- src/main/java/org/example/Terminator.java
+++ src/main/java/org/example/Terminator.java
@@ -1,25 +1,26 @@
 package org.example;
 
-import net.bytebuddy.description.method.MethodDescription;
-import net.bytebuddy.implementation.bytecode.StackManipulation;
-import net.bytebuddy.implementation.bytecode.constant.IntegerConstant;
-import net.bytebuddy.implementation.bytecode.constant.TextConstant;
-import net.bytebuddy.implementation.bytecode.member.MethodInvocation;
-import org.objectweb.asm.ClassReader;
-import org.objectweb.asm.ClassWriter;
-import org.objectweb.asm.tree.AbstractInsnNode;
-import org.objectweb.asm.tree.ClassNode;
-import org.objectweb.asm.tree.MethodInsnNode;
-import org.objectweb.asm.tree.MethodNode;
-import org.objectweb.asm.util.TraceClassVisitor;
-
+import java.io.File;
+import java.io.IOException;
 import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.Instrumentation;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
 import java.security.ProtectionDomain;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Terminator {
+    static final File LOG_OF_CLASSES = new File("classes_javaagent.txt");
+    static final List<String> CLASSES = new ArrayList<>();
+
+    static {
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            // Just to verify that the number of classes loaded in LOG_OF_CLASSES is correct
+            System.out.println("Classes loaded: " + List.copyOf(CLASSES).size());
+        }));
+    }
+
     public static void premain(String agentArgs, Instrumentation inst) {
         inst.addTransformer(
                 new ClassFileTransformer() {
@@ -36,45 +37,13 @@ public class Terminator {
     }
 
     private static byte[] terminationCode(String className, byte[] classfileBuffer) throws NoSuchMethodException {
-        String prohibitedClassName = "org/apache/commons/math3/analysis/function/Subtract";
-        if (className.equals("foo/Main")) {
-            ClassNode classNode = new ClassNode();
-            ClassReader classReader = new ClassReader(classfileBuffer);
-            classReader.accept(classNode, 0);
-            for (MethodNode method : classNode.methods) {
-                for (AbstractInsnNode insnNode: method.instructions) {
-                    // at each method call, check if the owner is the prohibited class
-                    if (insnNode instanceof MethodInsnNode) {
-                        if (((MethodInsnNode) insnNode).owner.equals(prohibitedClassName)) {
-                            AbstractInsnNode currNode = insnNode;
-
-                            List<StackManipulation> manipulations = new ArrayList<>();
-
-                            // add the code to print and exit
-                            manipulations.add(new TextConstant("You accidentally have executed `" + prohibitedClassName + "` in the app"));
-                            manipulations.add(MethodInvocation.invoke(new MethodDescription.ForLoadedMethod(Terminator.class.getMethod("printer", String.class))));
-                            manipulations.add(IntegerConstant.forValue(1));
-                            manipulations.add(MethodInvocation.invoke(new MethodDescription.ForLoadedMethod(Terminator.class.getMethod("exit", int.class))));
-
-                            StackManipulation operations = new StackManipulation.Compound(manipulations);
-
-                            currNode = ByteBuddyHelper.applyStackManipulation(method, currNode, operations, ByteBuddyHelper.InsertPosition.BEFORE);
-                        }
-                    }
-                }
-            }
-            ClassWriter writer = new ClassWriter(ClassWriter.COMPUTE_FRAMES);
-            classNode.accept(new TraceClassVisitor(writer, null));
-            return writer.toByteArray();
+        String s = className + "\n";
+        try {
+            Files.writeString(LOG_OF_CLASSES.toPath(), s, StandardOpenOption.CREATE, StandardOpenOption.APPEND);
+            CLASSES.add(className);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
         return classfileBuffer;
-    }
-
-    public static void exit(int status) {
-        System.exit(status);
-    }
-
-    public static void printer(String s) {
-        System.out.println(s);
     }
 }
