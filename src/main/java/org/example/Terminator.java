b0cb5ecfd5b1543fb8a080b7799121551dd5f105
Aman Sharma <mannu.poski10@gmail.com>
2023-07-11 18:58:38 +0200

    Add method to compare hash

--- src/main/java/org/example/Terminator.java
+++ src/main/java/org/example/Terminator.java
@@ -1,33 +1,31 @@
 package org.example;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.example.sbom.Cyclonedx;
 
-import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.Instrumentation;
-import java.nio.file.Files;
-import java.nio.file.StandardOpenOption;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.security.ProtectionDomain;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 public class Terminator {
-    static final File LOG_OF_CLASSES = new File("classes_javaagent.txt");
-    static final List<String> CLASSES = new ArrayList<>();
-
     private static Options OPTIONS;
 
+    private static final Map<String, String> classesToMD5Hash = new HashMap<>();
+
+    // Use build time statistics to determine the hash of the class
     static {
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            try {
-                Files.write(LOG_OF_CLASSES.toPath(), CLASSES, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }));
+        // Correct hash is acb694f66d6e4158ebf108d0f04338b1
+        classesToMD5Hash.put("io/github/chains_project/cs/commons/Pair", "acb694f66d6e4158ebf108d0f04338b");
+        classesToMD5Hash.put("foo/Main", "6d5ce21e2dc74790fe14ca3036cda67d");
     }
+
 
     public static void premain(String agentArgs, Instrumentation inst) {
         OPTIONS = new Options(agentArgs);
@@ -35,36 +33,53 @@ public class Terminator {
                 new ClassFileTransformer() {
                     @Override
                     public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer) {
-                        try {
-                            return terminationCode(className, classfileBuffer, protectionDomain);
-                        } catch (NoSuchMethodException e) {
-                            throw new RuntimeException(e);
-                        }
+                        return terminationCode(className, classfileBuffer, protectionDomain);
                     }
                 }
         );
     }
 
-    private static byte[] terminationCode(String className, byte[] classfileBuffer, ProtectionDomain protectionDomain) throws NoSuchMethodException {
+    private static byte[] terminationCode(String className, byte[] classfileBuffer, ProtectionDomain protectionDomain) {
         String jarPath;
-        Cyclonedx parsedSbom = parseSbom();
+        Cyclonedx parsedSbom = OPTIONS.getSbom();
         if (protectionDomain == null) {
-            jarPath = "null";
+            // we skip the bootstrap and platform classloader
+            return classfileBuffer;
         } else {
             jarPath = protectionDomain.getCodeSource().getLocation().getPath();
+            if (isHashOfClassCorrect(className, jarPath)) {
+                return classfileBuffer;
+            } else {
+                System.err.printf("Class %s is not in the SBOM%n", className);
+                System.exit(1);
+            }
         }
-        CLASSES.add(className + "," + jarPath);
-        return classfileBuffer;
+        return null;
     }
 
-    private static Cyclonedx parseSbom() {
-        ObjectMapper objectMapper = new ObjectMapper();
-        Cyclonedx root;
-        try {
-            root = objectMapper.readValue(OPTIONS.getSbomFile(), Cyclonedx.class);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+    private static boolean isHashOfClassCorrect(String className, String jarPath) {
+        try (JarFile jar = new JarFile(jarPath)) {
+            Enumeration<JarEntry> enumEntries = jar.entries();
+            while (enumEntries.hasMoreElements()) {
+                JarEntry classFileCandidate = enumEntries.nextElement();
+                if (String.format("%s.class", className).equals(classFileCandidate.getName())) {
+                    String md5 = computeMD5(jar.getInputStream(classFileCandidate));
+                    return classesToMD5Hash.get(className).equals(md5);
+                }
+            }
         }
-        return root;
+        catch (IOException | NoSuchAlgorithmException ex) {
+            throw new RuntimeException(ex);
+        }
+        return false;
+    }
+
+    private static String computeMD5(InputStream byteStream) throws NoSuchAlgorithmException, IOException {
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        byte[] hash = MessageDigest.getInstance("MD5").digest(byteStream.readAllBytes());
+        return new BigInteger(1, hash).toString(16);
     }
 }
+
+// 1 go to all purls in the sbom and check if the class name is in the maven central jar
+// 2 what would help me would be that I have all names of the classes in the sbom
