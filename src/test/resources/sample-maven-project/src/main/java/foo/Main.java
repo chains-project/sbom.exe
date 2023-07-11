fab786532a3ffee29a1faa3b46adf741ff35c95e
Aman Sharma <mannu.poski10@gmail.com>
2023-07-11 16:04:16 +0200

    Add third-party dependencies

--- src/test/resources/sample-maven-project/src/main/java/foo/Main.java
+++ src/test/resources/sample-maven-project/src/main/java/foo/Main.java
@@ -1,6 +1,9 @@
 package foo;
+
+import io.github.chains_project.cs.commons.Pair;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        Pair p = new Pair(1, 2);
     }
 }
