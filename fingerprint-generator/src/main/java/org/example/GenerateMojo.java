871999b5c8659e4675c5d7be70454bb624c9e1ee
Aman Sharma <mannu.poski10@gmail.com>
2023-07-13 17:45:42 +0200

    style: integrate spotless

--- fingerprint-generator/src/main/java/org/example/GenerateMojo.java
+++ fingerprint-generator/src/main/java/org/example/GenerateMojo.java
@@ -1,16 +1,15 @@
 package org.example;
 
-
 import org.apache.maven.plugin.AbstractMojo;
-import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.ResolutionScope;
 
 @Mojo(
-    name = "generate",
-    defaultPhase = LifecyclePhase.GENERATE_RESOURCES,
-    requiresDependencyResolution = ResolutionScope.COMPILE,
-    requiresOnline = true)
+        name = "generate",
+        defaultPhase = LifecyclePhase.GENERATE_RESOURCES,
+        requiresDependencyResolution = ResolutionScope.COMPILE,
+        requiresOnline = true)
 public class GenerateMojo extends AbstractMojo {
     public void execute() {
         System.out.println("Hello, world!");
