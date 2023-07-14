60fb0e6279be12e6c3f0848247a468d5c450c70a
Aman Sharma <mannu.poski10@gmail.com>
2023-07-14 14:42:47 +0200

    test: generate a bare minimum test

--- fingerprint-generator/src/test/java/org/example/it/GenerateMojoIT.java
+++ fingerprint-generator/src/test/java/org/example/it/GenerateMojoIT.java
@@ -6,6 +6,9 @@ import com.soebes.itf.jupiter.extension.MavenGoal;
 import com.soebes.itf.jupiter.extension.MavenJupiterExtension;
 import com.soebes.itf.jupiter.extension.MavenTest;
 import com.soebes.itf.jupiter.maven.MavenExecutionResult;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 @MavenJupiterExtension
 class GenerateMojoIT {
@@ -13,5 +16,22 @@ class GenerateMojoIT {
     @MavenTest
     void hello_world(MavenExecutionResult result) {
         assertThat(result).isSuccessful();
+    }
+
+    @MavenGoal("${project.groupId}:${project.artifactId}:${project.version}:generate")
+    @MavenTest
+    void guava(MavenExecutionResult result) throws IOException {
+        assertThat(result).isSuccessful();
+
+        Path projectDirectory = result.getMavenProjectResult().getTargetProjectDirectory();
+
+        Path targetDirectory = Path.of(projectDirectory.toString(), "target");
+        Path actualFingerprint = Path.of(targetDirectory.toString(), "classfile.sha-256");
+
+        Path expectedFingerprint =
+                Path.of(projectDirectory.toString(), "src", "test", "resources", "expected-classfile.sha256");
+        String expectedContent = Files.readString(expectedFingerprint);
+
+        assertThat(actualFingerprint).isRegularFile().hasContent(expectedContent);
     }
 }
