b4160bca90727c9e23786963ae5c5c1aca27d312
Aman Sharma <mannu.poski10@gmail.com>
2023-07-21 17:44:31 +0200

    docs: replicate fractureiser

---
+++ replications/fractureiser/hacker-server/Utility.java
@@ -0,0 +1,6 @@
+public class Utility {
+    public static void run(String args) {
+        System.out.println("From Utility.run: I am infecting you!");
+        System.out.println("From Utility.run: This was stage 0 of the infection.");
+    }
+}
