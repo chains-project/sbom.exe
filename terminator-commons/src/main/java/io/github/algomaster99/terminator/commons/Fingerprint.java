2120e147ec600d33f022cefee0cb525496bc28e7
Aman Sharma <mannu.poski10@gmail.com>
2023-07-27 14:26:14 +0200

    feat: include classfile version (#33)

--- terminator-commons/src/main/java/io/github/algomaster99/terminator/commons/Fingerprint.java
+++ terminator-commons/src/main/java/io/github/algomaster99/terminator/commons/Fingerprint.java
@@ -1,4 +1,10 @@
 package io.github.algomaster99.terminator.commons;
 
 public record Fingerprint(
-        String groupId, String artifactId, String version, String className, String hash, String algorithm) {}
+        String groupId,
+        String artifactId,
+        String version,
+        String className,
+        String classFileVersion,
+        String hash,
+        String algorithm) {}
--- terminator-commons/src/main/java/io/github/algomaster99/terminator/commons/HashComputer.java
+++ terminator-commons/src/main/java/io/github/algomaster99/terminator/commons/HashComputer.java
@@ -12,13 +12,17 @@ public class HashComputer {
         return toHexString(algorithmSum);
     }
 
-    private static String toHexString(byte[] bytes) {
+    public static String toHexString(byte[] bytes) {
         Formatter result = new Formatter();
         try (result) {
             for (byte b : bytes) {
-                result.format("%02x", b & 0xff);
+                result.format(toHexString(b));
             }
             return result.toString();
         }
+    }
+
+    public static String toHexString(byte b) {
+        return String.format("%02x", b & 0xff);
     }
 }
