e0000179dbb473332744903f1cf3f28078482e5e
Aman Sharma <mannu.poski10@gmail.com>
2023-08-01 14:17:24 +0200

    test: add more tests deserialisation

--- watchdog-agent/src/test/java/OptionsTest.java
+++ watchdog-agent/src/test/java/OptionsTest.java
@@ -2,6 +2,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import io.github.algomaster99.Options;
 import io.github.algomaster99.terminator.commons.fingerprint.Fingerprint;
+import io.github.algomaster99.terminator.commons.fingerprint.Jar;
 import io.github.algomaster99.terminator.commons.fingerprint.Maven;
 import java.io.File;
 import java.lang.reflect.InvocationTargetException;
@@ -15,12 +16,29 @@ public class OptionsTest {
     class ParseFingerprint {
         @Test
         void maven() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+            List<Fingerprint> fingerprints = deserializeFingerprints("src/test/resources/fingerprints/maven.jsonl");
+            assertThat(fingerprints).hasOnlyElementsOfType(Maven.class).size().isEqualTo(2);
+        }
+
+        @Test
+        void jar() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+            List<Fingerprint> fingerprints = deserializeFingerprints("src/test/resources/fingerprints/jar.jsonl");
+            assertThat(fingerprints).hasOnlyElementsOfType(Jar.class).size().isEqualTo(7);
+        }
+
+        @Test
+        void maven_jar() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+            List<Fingerprint> fingerprints = deserializeFingerprints("src/test/resources/fingerprints/maven_jar.jsonl");
+            assertThat(fingerprints).elements(0, 2).hasOnlyElementsOfType(Maven.class);
+            assertThat(fingerprints).elements(3, 5).hasOnlyElementsOfType(Jar.class);
+        }
+
+        private List<Fingerprint> deserializeFingerprints(String pathname)
+                throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
             Method method = Options.class.getDeclaredMethod("parseFingerprints", File.class);
             method.setAccessible(true);
-            Object result = method.invoke(null, new File("src/test/resources/fingerprints/maven.jsonl"));
-
-            List<Fingerprint> fingerprints = (List<Fingerprint>) result;
-            assertThat(fingerprints).hasOnlyElementsOfType(Maven.class).size().isEqualTo(2);
+            Object result = method.invoke(null, new File(pathname));
+            return (List<Fingerprint>) result;
         }
     }
 }
