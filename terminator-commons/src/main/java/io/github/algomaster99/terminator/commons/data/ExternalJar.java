26be7ee5208a9542844e199aef5629493cae5c5f
Aman Sharma <mannu.poski10@gmail.com>
2023-08-10 15:29:42 +0200

    fix: make the external jar path relative to config file

--- terminator-commons/src/main/java/io/github/algomaster99/terminator/commons/data/ExternalJar.java
+++ terminator-commons/src/main/java/io/github/algomaster99/terminator/commons/data/ExternalJar.java
@@ -1,5 +1,35 @@
 package io.github.algomaster99.terminator.commons.data;
 
+import com.fasterxml.jackson.core.JacksonException;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import java.io.File;
+import java.io.IOException;
 
+@JsonDeserialize(using = ExternalJarDeserialize.class)
 public record ExternalJar(File path) {}
+
+class ExternalJarDeserialize extends JsonDeserializer<ExternalJar> {
+
+    @Override
+    public ExternalJar deserialize(JsonParser jp, DeserializationContext ctx) throws IOException, JacksonException {
+        JsonNode node = jp.getCodec().readTree(jp);
+
+        String configFile = String.valueOf(ctx.findInjectableValue("configFile", null, null));
+
+        File relativePath = new File(node.get("path").asText());
+
+        if (!relativePath.isAbsolute()) {
+            relativePath = new File(configFile)
+                    .getParentFile()
+                    .toPath()
+                    .resolve(relativePath.toPath())
+                    .toFile();
+        }
+
+        return new ExternalJar(relativePath.getAbsoluteFile());
+    }
+}
