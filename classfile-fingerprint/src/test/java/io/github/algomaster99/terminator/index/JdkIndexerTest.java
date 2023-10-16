20739b5a0f4b6532a3cd3d897f61aff8081478dc
Aman Sharma <mannu.poski10@gmail.com>
2023-10-16 17:30:58 +0200

    tests: test for a different algorithm

--- classfile-fingerprint/src/test/java/io/github/algomaster99/terminator/index/JdkIndexerTest.java
+++ classfile-fingerprint/src/test/java/io/github/algomaster99/terminator/index/JdkIndexerTest.java
@@ -2,9 +2,13 @@ package io.github.algomaster99.terminator.index;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import io.github.algomaster99.terminator.commons.fingerprint.ParsingHelper;
+import io.github.algomaster99.terminator.commons.fingerprint.provenance.Provenance;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
+import java.util.Map;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
@@ -25,5 +29,20 @@ class JdkIndexerTest {
 
         // assert
         assertThat(contentFirst).isEqualTo(contentSecond);
+    }
+
+    @Test
+    void useMD5asAlgorithm(@TempDir Path tempDir) {
+        // arrange
+        Path indexFile = tempDir.resolve("jdk.json");
+
+        // act
+        String[] args = {"jdk", "-o", indexFile.toString(), "--algorithm", "MD5"};
+        Index.main(args);
+
+        // assert
+        Map<String, List<Provenance>> referenceProvenance = ParsingHelper.deserializeFingerprints(indexFile);
+        referenceProvenance.forEach((key, value) ->
+                assertThat(value.get(0).classFileAttributes().algorithm()).isEqualTo("MD5"));
     }
 }
