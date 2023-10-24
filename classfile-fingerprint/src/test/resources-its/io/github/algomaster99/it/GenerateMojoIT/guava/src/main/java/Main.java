8827446617afbfb26f6842eebe7c28eb52eddef8
Aman Sharma <mannu.poski10@gmail.com>
2023-10-24 22:57:36 +0200

    style: debloat classfile-fingerprint

--- classfile-fingerprint/src/test/resources-its/io/github/algomaster99/it/GenerateMojoIT/guava/src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-import com.google.common.base.Joiner;
-
-public class Main {
-    public static String main(String[] args) {
-        Joiner joiner = Joiner.on("; ").skipNulls();
-        return joiner.join("Harry", null, "Ron", "Hermione");
-    }
-}
--- classfile-fingerprint/src/test/resources-its/io/github/algomaster99/it/GenerateMojoIT/multi_module_with_sources/main/src/main/java/Main.java
+++
@@ -1,9 +0,0 @@
-import org.example.Pair;
-
-public class Main {
-    public static void main(String[] args) {
-        Pair pair = new Pair(1, 2);
-        System.out.println(pair.getFirst());
-        System.out.println(pair.getSecond());
-    }
-}
