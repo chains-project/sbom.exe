2682aedadfe886b0ca25e66291b09e5dfba941ce
Aman Sharma <mannu.poski10@gmail.com>
2024-02-25 19:19:23 +0100

    please spotless

--- classfile-fingerprint/src/test/java/io/github/algomaster99/terminator/preprocess/PomTransformerTest.java
+++ classfile-fingerprint/src/test/java/io/github/algomaster99/terminator/preprocess/PomTransformerTest.java
@@ -1,15 +1,14 @@
 package io.github.algomaster99.terminator.preprocess;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.IOException;
+import java.nio.file.Path;
 import org.apache.maven.model.Model;
 import org.apache.maven.model.Plugin;
 import org.codehaus.plexus.util.xml.Xpp3Dom;
 import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
 import org.junit.jupiter.api.Test;
-
-import java.io.IOException;
-import java.nio.file.Path;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 public class PomTransformerTest {
     @Test
@@ -48,7 +47,8 @@ public class PomTransformerTest {
         transformer.transform();
 
         // assert
-        assertThatSurefirePluginIsPresent(transformer.getModel().getBuild().getPlugins().get(0)) ;
+        assertThatSurefirePluginIsPresent(
+                transformer.getModel().getBuild().getPlugins().get(0));
     }
 
     @Test
@@ -56,7 +56,8 @@ public class PomTransformerTest {
         // arrange
         Path testPom = Path.of("src/test/resources/surefire/modify-argline.xml");
         PomTransformer transformer = new PomTransformer(testPom);
-        Xpp3Dom surefireConfiguration = (Xpp3Dom) transformer.getModel().getBuild().getPlugins().get(0).getConfiguration();
+        Xpp3Dom surefireConfiguration =
+                (Xpp3Dom) transformer.getModel().getBuild().getPlugins().get(0).getConfiguration();
         String originalArgLine = surefireConfiguration.getChild("argLine").getValue();
 
         // act
@@ -67,11 +68,13 @@ public class PomTransformerTest {
     }
 
     @Test
-    void modifySurefirePlugin_shouldAdd_failIfNoTests_and_testFailureIgnore() throws XmlPullParserException, IOException {
+    void modifySurefirePlugin_shouldAdd_failIfNoTests_and_testFailureIgnore()
+            throws XmlPullParserException, IOException {
         // arrange
         Path testPom = Path.of("src/test/resources/surefire/add-failIfNoTests_testFailureIgnore.xml");
         PomTransformer transformer = new PomTransformer(testPom);
-        Xpp3Dom originalSurefireConfiguration = (Xpp3Dom) transformer.getModel().getBuild().getPlugins().get(0).getConfiguration();
+        Xpp3Dom originalSurefireConfiguration =
+                (Xpp3Dom) transformer.getModel().getBuild().getPlugins().get(0).getConfiguration();
         assertThat(originalSurefireConfiguration).isNull();
 
         // act
@@ -79,24 +82,31 @@ public class PomTransformerTest {
 
         // assert
         Model transformedModel = transformer.getModel();
-        Xpp3Dom transformedSurefireConfiguration = (Xpp3Dom) transformedModel.getBuild().getPlugins().get(0).getConfiguration();
+        Xpp3Dom transformedSurefireConfiguration =
+                (Xpp3Dom) transformedModel.getBuild().getPlugins().get(0).getConfiguration();
         assertThat(transformedSurefireConfiguration.getChildCount()).isEqualTo(3);
 
-        String failIfNoTests = transformedSurefireConfiguration.getChild("failIfNoTests").getValue();
+        String failIfNoTests =
+                transformedSurefireConfiguration.getChild("failIfNoTests").getValue();
         assertThat(failIfNoTests).asBoolean().isFalse();
-        String testFailureIgnore = transformedSurefireConfiguration.getChild("testFailureIgnore").getValue();
+        String testFailureIgnore =
+                transformedSurefireConfiguration.getChild("testFailureIgnore").getValue();
         assertThat(testFailureIgnore).asBoolean().isTrue();
     }
 
     @Test
-    void modifySurefirePlugin_shouldModify_failIfNoTests_and_testFailureIgnore() throws XmlPullParserException, IOException {
+    void modifySurefirePlugin_shouldModify_failIfNoTests_and_testFailureIgnore()
+            throws XmlPullParserException, IOException {
         // arrange
         Path testPom = Path.of("src/test/resources/surefire/modify-failIfNoTests_testFailureIgnore.xml");
         PomTransformer transformer = new PomTransformer(testPom);
-        Xpp3Dom surefireConfiguration = (Xpp3Dom) transformer.getModel().getBuild().getPlugins().get(0).getConfiguration();
-        String originalFailIfNoTests = surefireConfiguration.getChild("failIfNoTests").getValue();
+        Xpp3Dom surefireConfiguration =
+                (Xpp3Dom) transformer.getModel().getBuild().getPlugins().get(0).getConfiguration();
+        String originalFailIfNoTests =
+                surefireConfiguration.getChild("failIfNoTests").getValue();
         assertThat(originalFailIfNoTests).asBoolean().isTrue();
-        String originalTestFailureIgnore = surefireConfiguration.getChild("testFailureIgnore").getValue();
+        String originalTestFailureIgnore =
+                surefireConfiguration.getChild("testFailureIgnore").getValue();
         assertThat(originalTestFailureIgnore).asBoolean().isFalse();
 
         // act
@@ -105,9 +115,11 @@ public class PomTransformerTest {
         // assert
         assertThat(surefireConfiguration.getChildCount()).isEqualTo(3);
 
-        String transformedFailIfNoTests = surefireConfiguration.getChild("failIfNoTests").getValue();
+        String transformedFailIfNoTests =
+                surefireConfiguration.getChild("failIfNoTests").getValue();
         assertThat(transformedFailIfNoTests).asBoolean().isFalse();
-        String transformedTestFailureIgnore = surefireConfiguration.getChild("testFailureIgnore").getValue();
+        String transformedTestFailureIgnore =
+                surefireConfiguration.getChild("testFailureIgnore").getValue();
         assertThat(transformedTestFailureIgnore).asBoolean().isTrue();
     }
 
