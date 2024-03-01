2477ed4b27225110ab1cd8f8c65cff586500ff3e
Aman Sharma <mannu.poski10@gmail.com>
2024-03-01 13:52:06 +0100

    chore: delete tests from watchdog-agent

--- watchdog-agent/src/test/resources/spoon-10.4.0/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    // This comment should be removed by the processor
-    public static void main(String[] args) {
-        // This also
-        System.out.println("Hello World!");
-    }
-    // This also
-}
