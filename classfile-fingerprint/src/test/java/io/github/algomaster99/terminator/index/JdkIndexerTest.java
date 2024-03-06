441ec6333feb671f8235a513ffb1952218a9dd44
Aman Sharma <mannu.poski10@gmail.com>
2024-03-06 19:55:50 +0100

    chore: add index for JDK 21.0.2+13 Temurin

--- classfile-fingerprint/src/test/java/io/github/algomaster99/terminator/index/JdkIndexerTest.java
+++ classfile-fingerprint/src/test/java/io/github/algomaster99/terminator/index/JdkIndexerTest.java
@@ -7,6 +7,7 @@ import io.github.algomaster99.terminator.commons.fingerprint.classfile.ClassFile
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import org.junit.jupiter.api.Test;
@@ -35,19 +36,19 @@ class JdkIndexerTest {
 
     @EnabledOnJre(JRE.JAVA_21)
     @Test
-    void jdkIndexShouldHave__jdk_internal_foreign_MemorySessionImpl__and__java_lang_foreign_MemorySegment$Scope(
-            @TempDir Path tempDir) {
+    void jdk21Index_shouldBeReproducibleAcrossMultiple_implementations(@TempDir Path tempDir) throws IOException {
         // arrange
-        Path indexFile = tempDir.resolve("jdk.json");
+        Path actualIndex = tempDir.resolve("jdk.jsonl");
+        Path expectedIndex = Path.of("src", "test", "resources", "jdk-index", "21.0.2-tem.jsonl");
 
         // act
-        String[] args = {"jdk", "-o", indexFile.toString()};
+        String[] args = {"jdk", "-o", actualIndex.toString()};
         Index.main(args);
 
         // assert
-        Map<String, Set<ClassFileAttributes>> referenceProvenance = ParsingHelper.deserializeFingerprints(indexFile);
-        assertThat(referenceProvenance)
-                .containsKeys("jdk/internal/foreign/MemorySessionImpl", "java/lang/foreign/MemorySegment$Scope");
+        List<String> actual = Files.readAllLines(actualIndex);
+        List<String> expected = Files.readAllLines(expectedIndex);
+        assertThat(actual).isEqualTo(expected);
     }
 
     @Test
