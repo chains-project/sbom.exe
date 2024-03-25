34003dc8a9de5172c07213b3132ffa552c01700d
Aman Sharma <mannu.poski10@gmail.com>
2024-03-25 20:03:16 +0100

    refactor: apply a supposedly idempotent operation

--- terminator-commons/src/main/java/io/github/algomaster99/terminator/commons/fingerprint/classfile/HashComputer.java
+++ terminator-commons/src/main/java/io/github/algomaster99/terminator/commons/fingerprint/classfile/HashComputer.java
@@ -3,12 +3,22 @@ package io.github.algomaster99.terminator.commons.fingerprint.classfile;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Formatter;
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassWriter;
 
 public class HashComputer {
     private HashComputer() {}
 
     public static String computeHash(byte[] bytes, String algorithm) throws NoSuchAlgorithmException {
-        byte[] algorithmSum = MessageDigest.getInstance(algorithm).digest(bytes);
+        ClassReader reader = new ClassReader(bytes);
+        ClassWriter writer = new ClassWriter(ClassWriter.COMPUTE_FRAMES);
+        reader.accept(writer, 0);
+
+        // apparently, ASM is not preserving the order of the constant pool table
+        // see: https://stackoverflow.com/questions/78221149/asm-does-not-preserve-order-in-the-constant-pool-table
+        byte[] modifiedBytesButShouldNotHaveBeen = writer.toByteArray();
+
+        byte[] algorithmSum = MessageDigest.getInstance(algorithm).digest(modifiedBytesButShouldNotHaveBeen);
         return toHexString(algorithmSum);
     }
 
