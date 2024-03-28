c9c38e8a8dd258a00978e4f84b92c1f0209c0dfc
Aman Sharma <mannu.poski10@gmail.com>
2024-03-28 18:36:43 +0100

    Copy the constant pool from original classfile

--- terminator-commons/src/main/java/io/github/algomaster99/terminator/commons/fingerprint/classfile/HashComputer.java
+++ terminator-commons/src/main/java/io/github/algomaster99/terminator/commons/fingerprint/classfile/HashComputer.java
@@ -3,12 +3,20 @@ package io.github.algomaster99.terminator.commons.fingerprint.classfile;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Formatter;
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassWriter;
 
 public class HashComputer {
     private HashComputer() {}
 
     public static String computeHash(byte[] bytes, String algorithm) throws NoSuchAlgorithmException {
-        byte[] algorithmSum = MessageDigest.getInstance(algorithm).digest(bytes);
+        ClassReader reader = new ClassReader(bytes);
+        ClassWriter writer = new ClassWriter(reader, 0);
+        reader.accept(writer, 0);
+
+        byte[] modifiedBytesButShouldNotHaveBeen = writer.toByteArray();
+
+        byte[] algorithmSum = MessageDigest.getInstance(algorithm).digest(modifiedBytesButShouldNotHaveBeen);
         return toHexString(algorithmSum);
     }
 
