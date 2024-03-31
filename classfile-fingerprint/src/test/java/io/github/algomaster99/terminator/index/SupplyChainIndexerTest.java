017c58f830631b64e2e1d051077788aebc82c69c
Aman Sharma <mannu.poski10@gmail.com>
2024-03-31 21:44:29 +0200

    Fix tests

--- classfile-fingerprint/src/test/java/io/github/algomaster99/terminator/index/SupplyChainIndexerTest.java
+++ classfile-fingerprint/src/test/java/io/github/algomaster99/terminator/index/SupplyChainIndexerTest.java
@@ -3,12 +3,11 @@ package io.github.algomaster99.terminator.index;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import io.github.algomaster99.terminator.commons.fingerprint.ParsingHelper;
-import io.github.algomaster99.terminator.commons.fingerprint.classfile.ClassFileAttributes;
+import io.github.algomaster99.terminator.commons.fingerprint.protobuf.Bomi;
+import io.github.algomaster99.terminator.commons.fingerprint.protobuf.ClassFile;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Map;
-import java.util.Set;
 import java.util.stream.Collectors;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
@@ -23,31 +22,14 @@ class SupplyChainIndexerTest {
         // act
         String[] argsFirst = {"supply-chain", "-s", sbom.toString(), "-o", indexFile.toString()};
         Index.main(argsFirst);
-        String contentFirst = Files.readString(indexFile);
+        byte[] contentFirst = Files.readAllBytes(indexFile);
 
         String[] argsSecond = {"supply-chain", "-s", sbom.toString(), "-i", indexFile.toString()};
         Index.main(argsSecond);
-        String contentSecond = Files.readString(indexFile);
+        byte[] contentSecond = Files.readAllBytes(indexFile);
 
         // assert
-        assertThat(contentFirst).isEqualTo(contentSecond);
-    }
-
-    @Test
-    void useMD5asAlgorithm(@TempDir Path tempDir) {
-        // arrange
-        Path indexFile = tempDir.resolve("sc.json");
-        Path sbom = Path.of("src", "test", "resources", "pdfbox-3.0.0.build-info-go-1.9.14.json");
-
-        // act
-        String[] args = {"supply-chain", "-s", sbom.toString(), "-o", indexFile.toString(), "--algorithm", "MD5"};
-        Index.main(args);
-
-        // assert
-        Map<String, Set<ClassFileAttributes>> referenceProvenance = ParsingHelper.deserializeFingerprints(indexFile);
-        assertThat(referenceProvenance.keySet().size()).isEqualTo(17790);
-        referenceProvenance.forEach((key, value) ->
-                assertThat(value.stream().findAny().get().algorithm()).isEqualTo("MD5"));
+        assertThat(Bomi.parseFrom(contentFirst)).isEqualTo(Bomi.parseFrom(contentSecond));
     }
 
     @Test
@@ -61,24 +43,24 @@ class SupplyChainIndexerTest {
         Index.main(args);
 
         // assert
-        Map<String, Set<ClassFileAttributes>> referenceProvenance = ParsingHelper.deserializeFingerprints(indexFile);
-        assertThat(referenceProvenance.keySet().size()).isEqualTo(1269);
+        Bomi referenceProvenance = ParsingHelper.deserializeFingerprints(indexFile);
+        assertThat(referenceProvenance.getClassFileCount()).isEqualTo(1269);
 
-        for (var referenceProvenanceEntry : referenceProvenance.entrySet()) {
-            if (referenceProvenanceEntry.getKey().equals("org/apache/logging/log4j/util/StackLocator")) {
-                assertThat(referenceProvenanceEntry.getValue()).size().isEqualTo(2);
-                assertThat(referenceProvenanceEntry.getValue().stream()
-                                .map(ClassFileAttributes::classfileVersion)
+        for (ClassFile referenceProvenanceEntry : referenceProvenance.getClassFileList()) {
+            if (referenceProvenanceEntry.getClassName().equals("org/apache/logging/log4j/util/StackLocator")) {
+                assertThat(referenceProvenanceEntry.getAttributeCount()).isEqualTo(2);
+                assertThat(referenceProvenanceEntry.getAttributeList().stream()
+                                .map(ClassFile.Attribute::getVersion)
                                 .collect(Collectors.toSet()))
                         .contains("52.0", "53.0");
             }
             // log4j-core has two versions of SystemClock
             // one as is and one in META-INF/versions/9/org/apache/logging/log4j/core/util/SystemClock
             // Even though they are kept in different directories, they have the same classfile version
-            if (referenceProvenanceEntry.getKey().equals("org/apache/logging/log4j/core/util/SystemClock")) {
-                assertThat(referenceProvenanceEntry.getValue()).size().isEqualTo(2);
-                assertThat(referenceProvenanceEntry.getValue().stream()
-                                .map(ClassFileAttributes::classfileVersion)
+            if (referenceProvenanceEntry.getClassName().equals("org/apache/logging/log4j/core/util/SystemClock")) {
+                assertThat(referenceProvenanceEntry.getAttributeCount()).isEqualTo(2);
+                assertThat(referenceProvenanceEntry.getAttributeList().stream()
+                                .map(ClassFile.Attribute::getVersion)
                                 .collect(Collectors.toSet()))
                         .contains("52.0");
             }
