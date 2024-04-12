3fefc756ccefc82c3fe7ef007a34325de127119c
Aman Sharma <mannu.poski10@gmail.com>
2024-04-12 16:03:32 +0200

    Add test

--- terminator-commons/src/test/java/io/github/algomaster99/terminator/commons/fingerprint/classfile/ConstantPoolParserTest.java
+++ terminator-commons/src/test/java/io/github/algomaster99/terminator/commons/fingerprint/classfile/ConstantPoolParserTest.java
@@ -2,6 +2,7 @@ package io.github.algomaster99.terminator.commons.fingerprint.classfile;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import io.github.algomaster99.terminator.commons.fingerprint.constant_pool.ConstantPoolParser;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -12,16 +13,40 @@ public class ConstantPoolParserTest {
     private static final Path CLASSFILE = Path.of("src/test/resources/classfile");
 
     @Test
+    void fooShouldBeConvertedToBar() throws IOException {
+        // arrange
+        Path foo = CLASSFILE.resolve("fooToBar").resolve("Foo.class");
+        byte[] fooBytes = Files.readAllBytes(foo);
+        String newName = "Bar";
+        byte[] barBytes = ConstantPoolParser.rewriteAllClassInfo(Files.readAllBytes(foo), newName);
+
+        // act
+        int fooLength = fooBytes.length;
+        int barLength = barBytes.length;
+
+        // assert
+        assertThat(fooLength).isNotEqualTo(barLength);
+        assertThat(fooLength
+                        - "java/lang/Object".getBytes().length // "Foo" extends "java/lang/Object"
+                        - "Foo".getBytes().length // "Foo" class name
+                        + newName.getBytes().length * 2) // "Bar" replaces both classes
+                .isEqualTo(barLength);
+    }
+
+    @Test
     void generatedConstructorAccessorIsRewritten() throws IOException {
+        // arrange
         Path gca5Runtime = CLASSFILE
                 .resolve("generated-constructor-accessor")
                 .resolve("GeneratedConstructorAccessor5Runtime.class");
-        String actualHash = HashComputer.computeHash(Files.readAllBytes(gca5Runtime));
-
         Path gca8Bomi =
                 CLASSFILE.resolve("generated-constructor-accessor").resolve("GeneratedConstructorAccessor8Bomi.class");
+
+        // act
+        String actualHash = HashComputer.computeHash(Files.readAllBytes(gca5Runtime));
         String expectedHash = HashComputer.computeHash(Files.readAllBytes(gca8Bomi));
 
+        // assert
         assertThat(actualHash).isEqualTo(expectedHash);
     }
 }
